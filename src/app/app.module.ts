0e3c054b469dcd45b6528197987f6feb02d9842e
DevEnock <enockomondi305@gmail.com>
2022-03-01 12:42:24 +0300

    adding project modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './components/book/book.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { BookEditComponent } from './components/book-edit/book-edit.component';
+import { BookCreateComponent } from './components/book-create/book-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookDetailComponent,
+    BookEditComponent,
+    BookCreateComponent
   ],
   imports: [
     BrowserModule,
